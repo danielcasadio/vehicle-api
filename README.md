1a4371a3b514bb40d95f52588a1596c4b1fb0c76
Daniel Casadio <danielcasadio@gmail.com>
2020-10-26 18:24:47 -0300

    Adição do update e README.md

--- README.md
+++ README.md
@@ -1,24 +1,49 @@
-# README
+# Vehicle API
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+### Esta aplicação é um exemplo de API Json RESTFUL 
 
-Things you may want to cover:
+---
 
-* Ruby version
+## Especficações 
 
-* System dependencies
+* Versão Ruby: 2.7.1
+* Versão Ruby on Rails: 6.0.3.4
 
-* Configuration
+---
 
-* Database creation
+## Funcionalidades 
 
-* Database initialization
+> As funcionalidades desta aplicação funcionam com end-points, para testar as mesmas, utilize a coleção [Vehicles](https://www.getpostman.com/collections/b5c09b8482f00f9a42b7) no Postman.
 
-* How to run the test suite
+ - GET/vehicles: Retorna todos os veículos
+ - GET/vehicles/{id}: Retorna os detalhes de um veículo específico
+ - POST/vehicles: Adiciona um novo veículo
+ - PUT/vehicles/{id}: Atualiza os dados de um veículo
+ - DELETE/vehicles/{id}: Apaga o veículo
 
-* Services (job queues, cache servers, search engines, etc.)
+--- 
 
-* Deployment instructions
+ ## Uso
 
-* ...
+ Considerando que já tenha as versões de Ruby e Ruby on Rails especificadas acima, para utilizar, é necessário clonar o repositório
+
+> ``` $ git clone https://github.com/danielcasadio/vehicle-api.git ``` 
+>
+>``` $ cd vehicle-api ``` 
+>
+>``` $ bundle ``` 
+>
+>``` $ rails db:create db:migrate ``` 
+>
+>``` $ rails server ```
+
+Após completo os passos, será possível testar as end-points pelo Postman.
+
+---
+
+## Contato e Dúvidas
+
+[Daniel Casadio](mailto:danielcasadio@gmail.com)
+
+
+
--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,41 +1,54 @@
-class Api::V1::VehiclesController < ApplicationController
-  def index
-    vehicles = Vehicle.order(title: :desc)
-    render json: {status: 'SUCCESS', message: 'Vehicles loaded', data: vehicles}, status: :ok
-  end
+module Api
+  module V1
+    class VehiclesController < ApplicationController
+      def index
+        vehicles = Vehicle.order(title: :desc)
+        render json: { status: 'SUCCESS', message: 'Vehicles loaded', data: vehicles }, status: :ok
+      end
 
-  def show 
-    vehicle = Vehicle.find(params[:id])
-    if vehicle  
-      render json: {status: 'SUCCESS', message: 'Vehicle loaded', data: vehicle}, status: :ok
-    else 
-      render json: {status: 'ERROR', message: 'Vehicle does not exist'}, status: :unprocessable_entity
+      def show
+        vehicle = Vehicle.find(params[:id])
+        if vehicle
+          render json: { status: 'SUCCESS', message: 'Vehicle loaded', data: vehicle }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Vehicle does not exist' }, status: :unprocessable_entity
+        end
+      end
+
+      def create
+        vehicle = Vehicle.new(vehicle_params)
+        if vehicle.save
+          render json: { status: 'SUCCESS', message: 'Vehicle created', data: vehicle }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cannot create the vehicle.', data: vehicle.errors.messages },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        vehicle = Vehicle.find(params[:id])
+        if vehicle.destroy
+          render json: { status: 'SUCCESS', message: 'Vehicle deleted' }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cannot delete vehicle' }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        vehicle = Vehicle.find(params[:id])
+        if vehicle.update(vehicle_params)
+          render json: { status: 'SUCCESS', message: 'Vehicle updated', data: vehicle }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cannot update vehicle', data: vehicle.errors.messages },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def vehicle_params
+        params.require(:vehicle).permit(:title, :brand, :year, :description, :sold)
+      end
     end
-  end
-
-  def create
-    vehicle = Vehicle.new(vehicle_params)
-    if vehicle.save
-      render json: {status: 'SUCCESS', message: 'Vehicle created', data: vehicle}, status: :ok
-    else
-      render json: {status: 'ERROR', message: 'Cannot create the vehicle.', data: vehicle.errors.messages}, status: :unprocessable_entity
-    end
-  end
-
-  def destroy 
-    vehicle = Vehicle.find(params[:id])
-    if vehicle && vehicle.destroy
-      render json: {status: 'SUCCESS', message: 'Vehicle deleted'}, status: :ok
-    else
-      render json: {status: 'ERROR', message: 'Cannot delete vehicle'}, status: :unprocessable_entity
-    end
-  end
-
-  private 
-
-  def vehicle_params 
-    params.require(:vehicle).permit(:title,:brand, :year,:description, :sold)
   end
 end
-
-
